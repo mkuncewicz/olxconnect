ec2480bc8adea08c0d79035543100a2cc9385443
Mateusz <kuncewicz.mateusz@gmail.com>
2024-11-15 23:46:27 +0100

    user

--- src/main/java/com/example/olxconnect/entity/User.java
+++ src/main/java/com/example/olxconnect/entity/User.java
@@ -1,4 +1,25 @@
 package com.example.olxconnect.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
 }
