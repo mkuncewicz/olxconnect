40cbf4b59bc887f10f8c646bfa9e6ae3507463ef
Mateusz <kuncewicz.mateusz@gmail.com>
2024-11-17 18:49:30 +0100

    idk

--- src/main/java/com/example/olxconnect/controller/HomeController.java
+++ src/main/java/com/example/olxconnect/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.example.olxconnect.controller;
 
+import com.example.olxconnect.entity.Token;
+import com.example.olxconnect.repository.TokenRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private TokenRepository tokenRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        List<Token> tokenList = tokenRepository.findAll();
+
+        model.addAttribute("tokenList", tokenList);
+
         return "home";
     }
 
