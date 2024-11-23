a1927cc8bc6147d5bb28e78f3508d037df0fea4d
Mateusz <kuncewicz.mateusz@gmail.com>
2024-11-23 21:53:58 +0100

    duzy update

---
+++ src/main/java/com/example/olxconnect/controller/HomeController.java
@@ -0,0 +1,50 @@
+package com.example.olxconnect.controller;
+
+import com.example.olxconnect.entity.Token;
+import com.example.olxconnect.repository.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        List<Token> tokenList = tokenRepository.findAll();
+
+        model.addAttribute("tokenList", tokenList);
+
+        return "home.html";
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Void> test(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 1;
+                while (true) {
+                    System.out.println("Test: " + i);
+                    i++;
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        thread.start();
+
+        return  ResponseEntity.ok().build();
+    }
+}
