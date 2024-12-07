345ca42aba8d2a9344f98ec2d7c135e330a308da
Mateusz <kuncewicz.mateusz@gmail.com>
2024-12-07 08:55:15 +0100

    advert

--- src/main/java/com/example/olxconnect/controller/AdvertController.java
+++ src/main/java/com/example/olxconnect/controller/AdvertController.java
@@ -3,6 +3,7 @@ package com.example.olxconnect.controller;
 import com.example.olxconnect.service.AdvertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class AdvertController {
     @Autowired
     private AdvertService advertService;
 
-    @PostMapping("/update")
+    @GetMapping("/update")
     public ResponseEntity<String> updateAdverts() {
         advertService.updateAdverts();
         return ResponseEntity.ok("Reklamy zostały zaktualizowane.");
