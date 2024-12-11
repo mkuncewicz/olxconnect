40c6e3035f410e0925aacceb2d6992b30d68dd79
Mateusz <kuncewicz.mateusz@gmail.com>
2024-12-11 14:50:54 +0100

    chat improved

--- src/main/java/com/example/olxconnect/controller/ChatController.java
+++ src/main/java/com/example/olxconnect/controller/ChatController.java
@@ -11,11 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller("/chat")
+@Controller
+@RequestMapping("/chat")
 public class ChatController {
 
     @Autowired
