cfbddd774a5fc2890d9172ffc6804e3ef9b1bdb5
Mateusz <kuncewicz.mateusz@gmail.com>
2024-12-30 02:57:37 +0100

    chat by refreshtoken

--- src/main/java/com/example/olxconnect/repository/TokenRepository.java
+++ src/main/java/com/example/olxconnect/repository/TokenRepository.java
@@ -2,6 +2,8 @@ package com.example.olxconnect.repository;
 
 import com.example.olxconnect.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,5 +15,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 
     Token findByRefreshToken(String refreshToken);
 
-    String findAccessTokenByRefreshToken(String refreshToken);
+    @Query("SELECT t.accessToken FROM Token t WHERE t.refreshToken = :refreshToken")
+    String findAccessTokenByRefreshToken(@Param("refreshToken") String refreshToken);
+
 }
